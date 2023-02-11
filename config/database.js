1de0bf86e7c897b7be3f7adc19cd89c68f1beea0
Kaen Pawanachai <pawaox4th@gmail.com>
2023-02-11 15:26:14 +0700

    feat: 🎸 Install `@faker-js/faker`

--- config/database.js
+++ config/database.js
@@ -8,6 +8,10 @@ function onCreateSSL(env) {
       user: env("DATABASE_USERNAME", "strapi"),
       password: env("DATABASE_PASSWORD", "strapi"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        // ca: env("DATABASE_CA"),
+      },
     };
   } else {
     return {
