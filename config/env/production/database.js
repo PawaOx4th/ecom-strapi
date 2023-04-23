edc58fc97adfda15a1cbe3b723d6764f7d27e76f
Kaen Pawanachai <pawaox4th@gmail.com>
2023-04-23 13:39:10 +0700

    ci: 🎡 update config database CA

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,17 +2,28 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
+      host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+      port: env("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "strapi"),
       password: env("DATABASE_PASSWORD", "strapi"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         // rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
         ca: env("DATABASE_CA"),
       },
     },
     debug: false,
+    acquireConnectionTimeout: 1000000,
+    pool: {
+      min: 0,
+      max: 100,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 50000,
+      idleTimeoutMillis: 300000,
+      reapIntervalMillis: 10000,
+      createRetryIntervalMillis: 2000,
+      propagateCreateError: false,
+    },
   },
 });
